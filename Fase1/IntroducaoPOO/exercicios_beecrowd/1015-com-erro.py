63720370cc4fdf281bdb5afdcd315c5c629c9c3f
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-08-28 13:43:30 -0300

    Create 1015-com-erro.py

---
+++ Fase1/IntroducaoPOO/exercicios_beecrowd/1015-com-erro.py
@@ -0,0 +1,12 @@
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+distancia = ((x2 -x1)**2 + (y2-y1)**2)**(1/2)
+print(f'{distancia:.4f}')
+
+# Traceback (most recent call last):
+# File "/judge/judge-2df49083357b453ea0296328545e5bb9.d/Main.py", line 1, in <module>
+#   x1 = float(input())
+#        ^^^^^^^^^^^^^^
+#ValueError: could not convert string to float: *******
