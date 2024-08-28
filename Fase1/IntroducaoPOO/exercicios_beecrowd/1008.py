19944b6000fc946d5163c550e8d2813b28a1c115
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-08-28 18:57:32 -0300

    Create 1008.py

---
+++ Fase1/IntroducaoPOO/exercicios_beecrowd/1008.py
@@ -0,0 +1,6 @@
+numero_funcionario = int(input())
+horas_trabalhadas = int(input())
+valor_hora = float(input())
+salario = valor_hora * horas_trabalhadas
+print(f'''NUMBER = {numero_funcionario}
+SALARY = U$ {salario:.2f}''')
